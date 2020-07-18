eff2bd4b7f506bc37f04428a81f67fbc57f5880f
cyw <18604469853@163.com>
2020-07-18 21:25:34 +0800

    Math p151

---
+++ API/src/Math/Demo_Math.java
@@ -0,0 +1,13 @@
+package Math;
+
+import java.util.Map;
+
+public class Demo_Math {
+
+    public static void main(String[] args) {
+        Math.abs(-1); // 绝对值
+        Math.ceil(1.5); // 向上取整
+        Math.floor(1.2); // 向下取整
+        Math.round(1.3); // 四舍五入
+    }
+}
